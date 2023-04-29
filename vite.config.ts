2a457b3f9d23890c080a41f6e79309065efe80e4
apo-star <apolle.world@gmail.com>
2023-04-29 19:28:14 +0300

    Include fonts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       scss: {
         additionalData: `
           @import "./src/styles/_mixins.scss";
+          @import "./src/styles/_fonts.scss";
         `,
       },
     },

00e1df2c17be6ceeff7897fda4756d6172eb3879
apo-star <apolle.world@gmail.com>
2023-04-29 14:32:49 +0300

    Add rem function

---
+++ src/components/Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: rem(1110);
+  padding: 0 rem(20);
+  margin: 0 auto;
+  box-sizing: content-box;
+}
+</style>
